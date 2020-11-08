be89ba23c2790045424efd103c6dfb64a525ad23
SalehKhaled7 <lost1994soul@gmail.com>
2020-11-08 20:06:20 +0300

    splash screen added , action and status bar removed on all activities

--- app/src/main/java/com/example/foodapp/HomeFragment.java
+++ app/src/main/java/com/example/foodapp/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.foodapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class HomeFragment extends Fragment {
-
+    Button donateBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v =  inflater.inflate(R.layout.fragment_home, container, false);
+        donateBtn = v.findViewById(R.id.donate_btn);
+        donateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),Donate.class));
+            }
+        });
+        return v;
     }
 }
