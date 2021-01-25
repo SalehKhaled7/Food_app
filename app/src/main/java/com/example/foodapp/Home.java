cf1e58f5501c375a78f8fd27afcf88e64c2e30d4
SalehKhaled7 <lost1994soul@gmail.com>
2021-01-25 18:34:18 +0300

    user profile update added

--- app/src/main/java/com/example/foodapp/Home.java
+++ app/src/main/java/com/example/foodapp/Home.java
@@ -11,12 +11,17 @@ import androidx.fragment.app.Fragment;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+import java.util.Objects;
 
 public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -24,6 +29,9 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     DrawerLayout drawer;
     ActionBarDrawerToggle toggle;
     Toolbar toolbar;
+    TextView username_tv;
+    TextView userEmail_tv;
+
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -34,12 +42,25 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         //getSupportActionBar().hide();// hide the action bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); //hide the status bar
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
         toolbar = findViewById(R.id.toolBar);
         toolbar.setTitle("Food Donation App");
         setSupportActionBar(toolbar);
 
         drawer=findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerLayout = navigationView.getHeaderView(0);
+
+        username_tv = headerLayout.findViewById(R.id.username_tv);
+        userEmail_tv = headerLayout.findViewById(R.id.userEmail_tv);
+        if (user != null){
+            String name = user.getDisplayName();
+            username_tv.setText(name);
+            String email = user.getEmail();
+            userEmail_tv.setText(email);
+        }
+
         navigationView.setNavigationItemSelectedListener(this);
         toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -48,6 +69,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 
         chipNavigationBar = findViewById(R.id.bottom_nav_menu);
         chipNavigationBar.setItemSelected(R.id.bottom_nav_home,true);
+
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
         bottomMenu();
 
@@ -55,27 +77,39 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        View main = findViewById(R.id.fragment_container);
+        View nav_main = findViewById(R.id.fragment_container2);
+        main.setVisibility(View.INVISIBLE);
         switch (item.getItemId()){
             case R.id.nav_profile:
                 toolbar.setTitle("Profile");
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ProfileFragment2()).commit();
+                nav_main.setVisibility(View.VISIBLE);
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container2,new ProfileFragment2()).commit();
                 break;
             case R.id.nav_address:
                 toolbar.setTitle("My address");
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new AddressFragment()).commit();
+                nav_main.setVisibility(View.VISIBLE);
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container2,new AddressFragment()).commit();
                 break;
             case R.id.nav_settings:
                 toolbar.setTitle("Settings");
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new SettingsFragment()).commit();
+                nav_main.setVisibility(View.VISIBLE);
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container2,new SettingsFragment()).commit();
                 break;
             case R.id.nav_about_us:
                 toolbar.setTitle("About us");
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new AboutUsFragment()).commit();
+                nav_main.setVisibility(View.VISIBLE);
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container2,new AboutUsFragment()).commit();
                 break;
             case R.id.nav_share:
-
                 Toast.makeText(this, "share", Toast.LENGTH_SHORT).show();
                 break;
+            case R.id.nav_home:
+                main.setVisibility(View.VISIBLE);
+                nav_main.setVisibility(View.INVISIBLE);
+
+                break;
+
 
         }
         return true;
--- app/src/main/java/com/example/foodapp/ProfileFragment2.java
+++ app/src/main/java/com/example/foodapp/ProfileFragment2.java
@@ -2,19 +2,99 @@ package com.example.foodapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.foodapp.models.UserHelperClass;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment2 extends Fragment {
 
 
+    DatabaseReference reference;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile2, container, false);
+        ImageView user_image = view.findViewById(R.id.user_image);
+        TextInputLayout user_display_name = view.findViewById(R.id.user_name_display_tv);
+        TextInputLayout user_Phone = view.findViewById(R.id.user_phone_tv);
+        Button user_info_update = view.findViewById(R.id.user_info_update);
+
+        UserHelperClass userHelperClass = new UserHelperClass();
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        reference = FirebaseDatabase.getInstance().getReference().child("users").child(user.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                user_Phone.getEditText().setText(snapshot.child("phoneNumber").getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        if (user != null){
+            if (user.getDisplayName() != null){
+                user_display_name.getEditText().setText(user.getDisplayName());
+            }
+        }
+
+
+        user_info_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reference.child("phoneNumber").setValue(user_Phone.getEditText().getText().toString());
+                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                        .setDisplayName(user_display_name.getEditText().getText().toString()).build();
+
+                user.updateProfile(profileUpdates)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getContext(), "profile updated successfully", Toast.LENGTH_SHORT).show();;
+                                }
+                            }
+                        });
+
+
+            }
+        });
+
+
+
+
+
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile2, container, false);
+        return view;
     }
 }
